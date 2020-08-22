5ec3cc22b8c23337cd87fd5445cf1be96c317bfe
bergmul <ulrich.bergmann@gmail.com>
2020-08-22 16:50:43 +0200

    Removed model regex hack from main table script

--- src/tables/table-main.R
+++ src/tables/table-main.R
@@ -19,7 +19,7 @@ option_list = list(
                default = NULL,
                help = "A directory path where models are saved",
                metavar = "character"),
-    make_option(c("-s", "--spec"),
+   make_option(c("-s", "--spec"),
                type = "character",
                default = NULL,
                help = "Input filename table spec",
@@ -30,32 +30,33 @@ option_list = list(
                help = "A regex of the models to load",
                metavar = "character"),
 	make_option(c("-o", "--out"),
-                type = "character",
-                default = "out.tex",
-                help = "output file name [default = %default]",
-                metavar = "character")
+               type = "character",
+               default = "out.tex",
+               help = "output file name [default = %default]",
+               metavar = "character")
 );
 
 opt_parser = OptionParser(option_list = option_list);
 opt = parse_args(opt_parser);
 
 if (is.null(opt$filepath)){
- print_help(opt_parser)
- stop("Input filepath must be provided", call. = FALSE)
+   print_help(opt_parser)
+   stop("Input filepath must be provided", call. = FALSE)
 }
 if (is.null(opt$spec)){
- print_help(opt_parser)
- stop("Input spec must be provided", call. = FALSE)
+   print_help(opt_parser)
+   stop("Input spec must be provided", call. = FALSE)
 }
 if (is.null(opt$models)){
- print_help(opt_parser)
- stop("A regex of model names must be provided", call. = FALSE)
+   print_help(opt_parser)
+   stop("A regex of model names must be provided", call. = FALSE)
 }
 
 # Load Files
 dir_path  <- opt$filepath
 s_name    <- opt$spec
 f_names   <- opt$models
+# TODO:  Fix Model Expression Hack - for now import all models
 models    <- paste0(dir_path, f_names)
 file_list <- Sys.glob(models)
 
@@ -64,8 +65,8 @@ model_names <- basename(tools::file_path_sans_ext(file_list))
 
 # Load into a list
 data <- file_list %>%
-            map(list.load) %>%
-            setNames(model_names)
+   map(list.load) %>%
+   setNames(model_names)
 
 # Create Table according to table spec
 source(s_name)
